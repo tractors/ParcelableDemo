cb7d894698808e4a9659b8025b81fce8d4400388
tractors <huijianhua@163.com>
2020-08-25 01:33:20 +0800

    first commit

---
+++ app/src/main/java/com/will/parcelabledemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.will.parcelabledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
